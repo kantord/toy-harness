b21443fe17ac62f96365b6b952f8efda3a52c329
Daniel Kantor <git@daniel-kantor.com>
2026-09-20 12:36:38 +0200

    add core schema

--- core/apps.py
+++ core/apps.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 
 
 class CoreConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
 
     def ready(self):
--- core/models.py
+++ core/models.py
@@ -1,2 +1,32 @@
+from django.db import models
 
-# Create your models here.
+
+class ModelProvider(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Model(models.Model):
+    name = models.CharField(max_length=255)
+    provider = models.ForeignKey(ModelProvider, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class WorkloadRunner(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Workload(models.Model):
+    enw = models.CharField(max_length=255, help_text='Enwiro environment name')
+    workload_runner = models.ForeignKey(WorkloadRunner, on_delete=models.CASCADE)
+    model = models.ForeignKey(Model, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.enw
